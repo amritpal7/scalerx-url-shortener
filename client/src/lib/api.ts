d8b2ba6845aa153cbf4a2831a4c55d76f5e9d1bf
Amritpal Singh <35212838+amritpal7@users.noreply.github.com>
2025-08-07 15:22:45 +0530

    prod variable

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.MODE === "production"
+      ? import.meta.env.VITE_PROD_API_URL
+      : import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
