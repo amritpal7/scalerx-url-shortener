9ebe7f2b84b7a4aa887741b4093b3e346cb34e92
Amritpal Singh <35212838+amritpal7@users.noreply.github.com>
2025-08-14 22:44:49 +0530

    cookies config

--- server/src/utils/cookieTokens.ts
+++ server/src/utils/cookieTokens.ts
@@ -8,14 +8,14 @@ export function setCookie(
   res.cookie("refreshToken", token.refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
   // store token in cookie
   res.cookie("accessToken", token.accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 15 * 60 * 1000, // 15 mins
   });
 }
